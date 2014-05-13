1f1c08971fe26af568e899f8f2a40d0aff11b7db
testcourse <jguo2@jguo2-WS.cisco.com>
2014-05-13 22:21:06 +0800

    commit

--- README.md
+++ README.md
@@ -1,21 +1,60 @@
-course
-======
+Course Project for Getting and Cleaning Data
+========================================================
+## Merges the training and the test sets to create one data set.
+
+
+```r
+## Load features and name total X data column with feature
+features <- read.table("UCI HAR Dataset/features.txt", header = FALSE)
+## Load and combine all X data, then name its columns
+dataTrainX <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+dataTestX <- read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+totalX <- rbind(dataTrainX, dataTestX)
+names(totalX) <- as.character(features[, 2])
+## Load and combine all y data, then name its columns
+dataTrainy <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
+dataTesty <- read.table("UCI HAR Dataset/test/y_test.txt", header = FALSE)
+totaly <- rbind(dataTrainy, dataTesty)
+names(totaly) <- c("ActivityID")
+## Load activities and set their column name
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", head = FALSE)
+names(activities) <- c("ActivityID", "Activity")
+## Merge y data with activities data to get meaningful label
+totaly <- merge(totaly, activities, by.x = "ActivityID", by.y = "ActivityID", 
+    all = FALSE)
+## Load, combine and name all the subject
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+subject <- rbind(subjectTrain, subjectTest)
+names(subject) <- c("Subject")
+## Combine the whole data set into a single dataset name set1
+set1 <- cbind(totalX, totaly, subject)
+```
 
 
 
-1. Load all train_X data to dataTrainX
-2. Load all train_y data to dataTrainy
-3. Load all test_X data to dataTestX
-4. Load all test_y data to dataTesty
-5. Load all features data to features
-6. Load all activities data to activities
-7. Load all train subject data to subjectTrain
-8. Load all test subject data to subjectTest
-9. Merge all subject data to subject
-10.Transfer subject to character
-11.Merge all X data to totalX
-12.Merge all y data to totaly
-13.Set name of totalX with features
-14.Get index of features who contains "avg()" or "std()"
-15.Merge data with activity, subject,features
-16.Get tidy data
+## Extract only the measurements on the mean and standard deviation for each measurement
+
+```r
+originalindex <- grep("mean\\(\\)|std\\(\\)", names(set1))
+set2 <- set1[, c(originalindex, 563, 564)]
+```
+
+## Use the descriptive names
+
+
+```r
+names(set2) <- gsub("[-()]", "", names(set2))
+```
+
+
+## Tidy dataset with the average of each variable for each activity and each subject
+
+```r
+tidyDataSet <- aggregate(set2[, 1:66], by = list(Subject = set2$Subject, Activity = set2$Activity), 
+    mean)
+write.csv(tidyDataSet, file = "tidy.csv", row.names = FALSE)
+```
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,36 @@
-dataTrainX <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE);
-dataTrainy <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE);
-dataTestX <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE);
-dataTesty <- read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE);
+## Load features and name total X data column with feature
 features <- read.table("UCI HAR Dataset/features.txt", header=FALSE);
+## Load and combine all X data, then name its columns
+dataTrainX <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE);
+dataTestX <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE);
+totalX <- rbind(dataTrainX,dataTestX);
+names(totalX) <- as.character(features[,2]);
+## Load and combine all y data, then name its columns
+dataTrainy <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE);
+dataTesty <- read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE);
+totaly <- rbind(dataTrainy,dataTesty);
+names(totaly) <- c("ActivityID");
+## Load activities and set their column name
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", head=FALSE);
+names(activities) <- c("ActivityID","Activity");
+## Merge y data with activities data to get meaningful label
+totaly <- merge(totaly, activities, by.x="ActivityID", by.y="ActivityID",all=FALSE);
+## Load, combine and name all the subject
 subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE);
 subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE);
 subject <- rbind(subjectTrain,subjectTest);
-featurenames <- as.character(features[,2]);
-totalX <- rbind(dataTrainX,dataTestX);
-totaly <- rbind(dataTrainy,dataTesty);
-names(totalX) <- featurenames;
-tidyindex <- grep("mean\\(\\)|std\\(\\)",featurenames);
-tidyX <- totalX[,tidyindex];
-entireactivity <- merge(totaly,activities,by.x="V1",by.y="V1",all.x=TRUE,all.y=FALSE);
-tmp <- data.frame(subject=subject$V1,activity=entireactivity$V2);
-tidyData <- cbind(tmp,tidyX);
-tidy <- tidyData[1:180,];
-for (i in 1:30){
-  for (j in 1:6){
-    for (k in 3:ncol(tidyData)){
-      for (l in 1:180){
-        tidy[l,k] <- tapply(tidyData[,k],subject == i & as.integer(activity) == j,mean)[2];
-      }
-    }
-  }
-}   
+names(subject) <- c("Subject");
+## Combine the whole data set into a single dataset name set1
+set1 <- cbind(totalX,totaly,subject);
 
+## Extract only the measurements on the mean and standard deviation for each measurement
+originalindex <- grep("mean\\(\\)|std\\(\\)",names(set1));
+set2 <- set1[,c(originalindex,563,564)];
+
+## Use the descriptive names
+names(set2) <- gsub("[-()]","",names(set2));
+
+## Tidy dataset with the average of each variable for each activity and each subject
+tidyDataSet <- aggregate(set2[,1:66], by=list(Subject=set2$Subject,Activity=set2$Activity),mean);
+write.csv(tidyDataSet, file="tidy.csv",row.names=FALSE);
 
